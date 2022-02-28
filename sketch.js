cd4397266666251e2ed86a1a9ad637c3ae10b66a
mattexpert09 <93745342+mattexpert09@users.noreply.github.com>
2022-02-28 17:53:13 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,11 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1;
 var box3;
+var box4;
+var box5;
+var box6;
+var box7;
+var box8;
 
 function setup(){
     var canvas = createCanvas(400,400);
@@ -14,6 +19,12 @@ function setup(){
     box3=new Box(200,300,50,50);
     box1 = new Box(200,300,50,50);
     box2 = new Box(240,100,50,100);
+    box4 = new Box(200,300,50,50);
+    box5 = new Box(240,100,50,100);
+    box6 = new Box(240,100,50,100);
+    box7 = new Box(240,100,50,100);
+    box8 = new Box(240,100,50,100); 
+    
     ground = new Ground(200,height,400,20)
 }
 
@@ -25,6 +36,13 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
-    box3.display();
+    box3.display(); 
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+    box8.display();
+ 
+    
     ground.display();
 }
