e044e2bfd1a59fac1943d56d646c5cf042247861
mattexpert09 <93745342+mattexpert09@users.noreply.github.com>
2022-02-28 17:40:02 -0600

    Update and rename sketch.js to sketch.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,14 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1;
+var box3;
 
 function setup(){
     var canvas = createCanvas(400,400);
     engine = Engine.create();
     world = engine.world;
 
+    box3=new Box(200,300,50,50);
     box1 = new Box(200,300,50,50);
     box2 = new Box(240,100,50,100);
     ground = new Ground(200,height,400,20)
@@ -23,5 +25,6 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
+    box3.display();
     ground.display();
 }
